e6ac71f2f5839760322e50cad64ed98dc733d7c2
Cyberlane <dogbertuk2000@gmail.com>
2014-04-13 19:52:08 +0100

    Initial config file setup

---
+++ src/Camera-Snapper/ConfigSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camera_Snapper
+{
+    public class ConfigSettings
+    {
+        public string SnapshotPath { get; set; }
+    }
+}
--- src/Camera-Snapper/Form1.cs
+++ src/Camera-Snapper/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,56 @@ namespace Camera_Snapper
 {
     public partial class Form1 : Form
     {
+        private string _settingsPath;
+        private string _snapshotsPath;
+        private const string FolderName = "Camera-Snapper";
+
         public Form1()
         {
             InitializeComponent();
+            InitialiseConfigFile();
+        }
+
+        private void InitialiseConfigFile()
+        {
+            _snapshotsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), FolderName);
+            if (!Directory.Exists(_snapshotsPath))
+            {
+                Directory.CreateDirectory(_snapshotsPath);
+            }
+            ConfigSettings settings;
+            _settingsPath = Path.Combine(_snapshotsPath, "setting.json");
+            if (!File.Exists(_settingsPath))
+            {
+                _snapshotsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), FolderName);
+                Directory.CreateDirectory(_snapshotsPath);
+                settings = new ConfigSettings
+                {
+                    SnapshotPath = _snapshotsPath
+                };
+            }
+            else
+            {
+                settings = LoadSettings();
+                _snapshotsPath = settings.SnapshotPath;
+                if (!Directory.Exists(_snapshotsPath))
+                {
+                    Directory.CreateDirectory(_snapshotsPath);
+                }
+            }
+            UpdateSettings(settings);
+        }
+
+        private ConfigSettings LoadSettings()
+        {
+            var raw = File.ReadAllText(_settingsPath);
+            return JsonConvert.DeserializeObject<ConfigSettings>(raw);
+        }
+
+        private void UpdateSettings(ConfigSettings config)
+        {
+            var raw = JsonConvert.SerializeObject(config);
+            File.WriteAllText(_settingsPath, raw);
         }
     }
 }
